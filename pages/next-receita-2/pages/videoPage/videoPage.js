995877362bbd3666ba4492f7528a387c0ae4aec8
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-11-20 10:22:26 -0300

    mudando o video

--- pages/next-receita-2/pages/videoPage/videoPage.js
+++ pages/next-receita-2/pages/videoPage/videoPage.js
@@ -5,7 +5,7 @@ export default function PlayVideo () {
   return (
     <div className={styles.video}>
       {/* <div className={styles.videoDiv}>aaaa</div> */}
-      <YouTubeBackgroundTwo videoLink={`https://www.youtube.com/embed/Rz2SNm8VguE?autoplay=1&loop=1&controls=0&showinfo=0&mute=1`}/>
+      <YouTubeBackgroundTwo videoLink={`https://www.youtube.com/embed/rdix1XxaZyU?autoplay=1&loop=1&controls=0&showinfo=0&mute=1`}/>
     </div>
   );
 }
