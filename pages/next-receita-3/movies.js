c5b6175f95ae43a2d99fdf768fe1bf606a440913
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-11-29 13:37:25 -0300

    adicionando um comentário na receita 3

--- pages/next-receita-3/movies.js
+++ pages/next-receita-3/movies.js
@@ -2,6 +2,8 @@
 // O await serve para que o programa aguarde o resultado da função para prosseguir.
 // No NextJS, se queremos que um código seja executado no servidor quando uma página é requisitada, precisamos exportar uma função assíncrona de nome getServerSideProps, que recebe um objeto context como parâmetro e que retorna um objeto JSON com uma propriedade de nome props.
 
+
+// Faz a requisição dos dados da API e os transforma em JSON
 export async function getServerSideProps(context){
     const res = await fetch(`https://www.omdbapi.com/?apikey=5a35dc18&s=bagdad`)
     const data = await res.json()
