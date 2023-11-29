d9af4271b35e8365f0fcc09d367e5fcc8ea04236
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-11-29 10:10:29 -0300

    adicionando o getServerSideProps receita 3

--- pages/next-receita-3/movies.js
+++ pages/next-receita-3/movies.js
@@ -1,3 +1,22 @@
+// A função fetch faz uma requisição, a uma máquina remota, de um recurso descrito numa URL, via http.
+// O await serve para que o programa aguarde o resultado da função para prosseguir.
+
+export async function getServerSideProps(context){
+    const res = await fetch(`https://www.omdbapi.com/?apikey=5a35dc18&s=bagdad`)
+    const data = await res.json()
+    
+    return {
+        
+        props: {
+            
+            data
+            
+        }
+        
+    }
+    
+}
+
 function transformar (elemento){
     return(<div>{elemento.Title} --- {elemento.Year}</div>)
 }
@@ -6,87 +25,87 @@ export default function Movies() {
 
 
 
-    const data = {
+    // const data = {
 
-        "Search": [
+    //     "Search": [
 
-            {
+    //         {
 
-                "Title": "Bagdad Café",
+    //             "Title": "Bagdad Café",
 
-                "Year": "1987",
+    //             "Year": "1987",
 
-                "imdbID": "tt0095801",
+    //             "imdbID": "tt0095801",
 
-                "Type": "movie",
+    //             "Type": "movie",
 
-                "Poster": "https://m.media-amazon.com/images/M/MV5BNmUwZGZhZTktNGNmOS00NDA0LWEzYWUtZjE2ODA5OTM2OTc2XkEyXkFqcGdeQXVyMTQ3Njg3MQ@@._V1_SX300.jpg"
+    //             "Poster": "https://m.media-amazon.com/images/M/MV5BNmUwZGZhZTktNGNmOS00NDA0LWEzYWUtZjE2ODA5OTM2OTc2XkEyXkFqcGdeQXVyMTQ3Njg3MQ@@._V1_SX300.jpg"
 
-            },
+    //         },
 
-            {
+    //         {
 
-                "Title": "The Thief of Bagdad",
+    //             "Title": "The Thief of Bagdad",
 
-                "Year": "1940",
+    //             "Year": "1940",
 
-                "imdbID": "tt0033152",
+    //             "imdbID": "tt0033152",
 
-                "Type": "movie",
+    //             "Type": "movie",
 
-                "Poster": "https://m.media-amazon.com/images/M/MV5BZWFhYjg4NTEtY2IzMS00YTc2LTg1NGUtMTEzNDBlZDIxZTk3XkEyXkFqcGdeQXVyMTMxMTY0OTQ@._V1_SX300.jpg"
+    //             "Poster": "https://m.media-amazon.com/images/M/MV5BZWFhYjg4NTEtY2IzMS00YTc2LTg1NGUtMTEzNDBlZDIxZTk3XkEyXkFqcGdeQXVyMTMxMTY0OTQ@._V1_SX300.jpg"
 
-            },
+    //         },
 
-            {
+    //         {
 
-                "Title": "Bagdad",
+    //             "Title": "Bagdad",
 
-                "Year": "1949",
+    //             "Year": "1949",
 
-                "imdbID": "tt0041149",
+    //             "imdbID": "tt0041149",
 
-                "Type": "movie",
+    //             "Type": "movie",
 
-                "Poster": "https://m.media-amazon.com/images/M/MV5BZjAxYTJmMmItYzZhMS00OWMwLTkxMjEtZjVmNmM3ZGRlZTljXkEyXkFqcGdeQXVyNTk1MTk0MDI@._V1_SX300.jpg"
+    //             "Poster": "https://m.media-amazon.com/images/M/MV5BZjAxYTJmMmItYzZhMS00OWMwLTkxMjEtZjVmNmM3ZGRlZTljXkEyXkFqcGdeQXVyNTk1MTk0MDI@._V1_SX300.jpg"
 
-            },
+    //         },
 
-            {
+    //         {
 
-                "Title": "Bowery to Bagdad",
+    //             "Title": "Bowery to Bagdad",
 
-                "Year": "1955",
+    //             "Year": "1955",
 
-                "imdbID": "tt0044388",
+    //             "imdbID": "tt0044388",
 
-                "Type": "movie",
+    //             "Type": "movie",
 
-                "Poster": "https://m.media-amazon.com/images/M/MV5BZTMwMTUyZTktOTk4OC00ZGY2LWFmMzAtYjkxMWMxNjdmOTg4XkEyXkFqcGdeQXVyNDY3MzU2MDM@._V1_SX300.jpg"
+    //             "Poster": "https://m.media-amazon.com/images/M/MV5BZTMwMTUyZTktOTk4OC00ZGY2LWFmMzAtYjkxMWMxNjdmOTg4XkEyXkFqcGdeQXVyNDY3MzU2MDM@._V1_SX300.jpg"
 
-            },
+    //         },
 
-            {
+    //         {
 
-                "Title": "Babes in Bagdad",
+    //             "Title": "Babes in Bagdad",
 
-                "Year": "1952",
+    //             "Year": "1952",
 
-                "imdbID": "tt0044389",
+    //             "imdbID": "tt0044389",
 
-                "Type": "movie",
+    //             "Type": "movie",
 
-                "Poster": "https://m.media-amazon.com/images/M/MV5BYjBmNmRlNzAtZDNjYi00ZjQ0LTljYTktZGUxOGNhMjFhZWI1XkEyXkFqcGdeQXVyMTE2NzA0Ng@@._V1_SX300.jpg"
+    //             "Poster": "https://m.media-amazon.com/images/M/MV5BYjBmNmRlNzAtZDNjYi00ZjQ0LTljYTktZGUxOGNhMjFhZWI1XkEyXkFqcGdeQXVyMTE2NzA0Ng@@._V1_SX300.jpg"
 
-            }
+    //         }
 
-        ],
+    //     ],
 
-        "totalResults": "59",
+    //     "totalResults": "59",
 
-        "Response": "True"
+    //     "Response": "True"
 
-    }
+    // }
 
     return (
 
