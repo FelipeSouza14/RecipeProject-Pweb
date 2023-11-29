2d195c52aec270d55d6e9f93e56eacc6774c5fa3
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-11-29 10:21:02 -0300

    adicionando alguns comentários

--- pages/next-receita-3/movies.js
+++ pages/next-receita-3/movies.js
@@ -1,12 +1,13 @@
 // A função fetch faz uma requisição, a uma máquina remota, de um recurso descrito numa URL, via http.
 // O await serve para que o programa aguarde o resultado da função para prosseguir.
+// No NextJS, se queremos que um código seja executado no servidor quando uma página é requisitada, precisamos exportar uma função assíncrona de nome getServerSideProps, que recebe um objeto context como parâmetro e que retorna um objeto JSON com uma propriedade de nome props.
 
 export async function getServerSideProps(context){
     const res = await fetch(`https://www.omdbapi.com/?apikey=5a35dc18&s=bagdad`)
     const data = await res.json()
     
     return {
-        
+        // O json precisa estar encapsulado em props, pois é regra do Next.js
         props: {
             
             data // Como foi usado o nome "data" aqui...
