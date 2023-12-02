d23d7c9e26f386a640720ab01cd28f356b53a030
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-12-02 20:57:11 -0300

    adicionando a pesquisa

--- pages/next-receita-3/movies.js
+++ pages/next-receita-3/movies.js
@@ -1,3 +1,5 @@
+import styles from '../styles/receita3/movies.module.css';
+import React from "react";
 // A função fetch faz uma requisição, a uma máquina remota, de um recurso descrito numa URL, via http.
 // O await serve para que o programa aguarde o resultado da função para prosseguir.
 // No NextJS, se queremos que um código seja executado no servidor quando uma página é requisitada, precisamos exportar uma função assíncrona de nome getServerSideProps, que recebe um objeto context como parâmetro e que retorna um objeto JSON com uma propriedade de nome props.
@@ -22,9 +24,21 @@ function transformar (elemento){
 }
 
 export default function Movies({data}) { // Então o nome do parametro desestruturado também precisa ser igual
+    const [search, setSearch] = React.useState("");
+
+    const searchLowerCase = search.toLocaleLowerCase();
+    const dataSearch = data.Search.filter((p) => p.Title.toLocaleLowerCase().includes(searchLowerCase));
+
     return (
-        <div>
-            {data.Search.map(transformar)}
+        <div className={styles.pageBackground}>
+            <input
+                className={styles.searchBar}
+                type="text"
+                placeholder="pesquise seu filme"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            {dataSearch.map(transformar)}
         </div>
     )
 }
