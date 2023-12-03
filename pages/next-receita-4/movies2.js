418e75904946395af8a8f126e46c92e41e49fa90
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-12-02 22:04:32 -0300

    add o script r4, instalando a lib swr

--- pages/next-receita-4/movies2.js
+++ pages/next-receita-4/movies2.js
@@ -1,1 +1,18 @@
 import styles from '../styles/receita4/movies2.module.css';
+
+export default function Movies2() {
+    const data = fetcher(`http://www.omdbapi.com/?apikey=ME_SUBSTITUA&s=bagdad`)
+
+    return (
+        <div>
+            {data.Search.map((m) => <div>{m.Title} --- {m.Year}</div>)}
+        </div>
+    )
+}
+
+async function fetcher(url) {
+    const res = await fetch(url);
+    const json = await res.json();
+
+    return json;
+}
