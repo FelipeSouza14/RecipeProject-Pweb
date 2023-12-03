7aa107223ee327663b69fde532d4fb3fbdbdb894
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-12-02 21:25:26 -0300

    ajeitando o css r3

--- pages/next-receita-3/movies.js
+++ pages/next-receita-3/movies.js
@@ -6,31 +6,42 @@ import React from "react";
 
 
 // Faz a requisição dos dados da API e os transforma em JSON
-export async function getServerSideProps(context){
+export async function getServerSideProps(context) {
     const res = await fetch(`https://www.omdbapi.com/?apikey=5a35dc18&s=bagdad`)
     const data = await res.json()
-    
+
     return {
         // O json precisa estar encapsulado em props, pois é regra do Next.js
         props: {
-            
+
             data // Como foi usado o nome "data" aqui...
         }
     }
 }
 
-function transformar (elemento){
-    return(<div className={styles.cards}>{elemento.Title} --- {elemento.Year}</div>)
+function transformar(elemento) {
+    return (
+        <div className={styles.cards}>
+            <img src={elemento.Poster} className={styles.stylePoster} />
+            <div className={styles.description}>
+                {elemento.Title}
+                <br />
+                {elemento.Year}
+            </div>
+        </div>
+    )
 }
 
-export default function Movies({data}) { // Então o nome do parametro desestruturado também precisa ser igual
+export default function Movies({ data }) { // Então o nome do parametro desestruturado também precisa ser igual
     const [search, setSearch] = React.useState("");
 
     const searchLowerCase = search.toLocaleLowerCase();
-    const dataSearch = data.Search.filter((p) => { return (
-        p.Title.toLocaleLowerCase().includes(searchLowerCase) ||
-        p.Year.includes(searchLowerCase)
-    )});
+    const dataSearch = data.Search.filter((p) => {
+        return (
+            p.Title.toLocaleLowerCase().includes(searchLowerCase) ||
+            p.Year.includes(searchLowerCase)
+        )
+    });
 
     return (
         <div className={styles.pageBackground}>
